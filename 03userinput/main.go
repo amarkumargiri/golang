26a1588e17e085df0c1de750c201c9cb5d02dfca
amargiri1717@gmail.com <amargiri1717@gmail.com>
2026-06-20 12:04:46 +0530

    Add initial go.mod and main.go files for user input and comma ok demonstration

---
+++ 03userinput/main.go
@@ -0,0 +1,22 @@
+package main
+
+import(
+	"bufio";
+	"fmt";
+	"os"
+)
+
+func main(){
+	welcome := "welcome to the user input"
+	fmt.Println(welcome)
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter the rating of pizza")
+
+	//comma ok
+
+	input, _ := reader.ReadString('\n')
+	fmt.Println("Thanks for input ", input)
+	fmt.Printf("Type of this rating is %T\n", input)
+
+}
